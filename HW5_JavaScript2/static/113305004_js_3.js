0702365b92e9ec150354cb5d8b38662d6ab1b263
Y0uchengLin <benny.youcheng@gmail.com>
2025-10-02 14:10:45 +0800

    新增計時功能

---
+++ HW5_JavaScript2/static/113305004_js_3.js
@@ -0,0 +1,28 @@
+var ans = Math.floor(Math.random() * 101);
+var guesstime = 1;
+var startTime = null;
+
+function guess() {
+    let inp = parseInt(document.getElementById("input_1").value);
+
+    if (guesstime === 1) {
+        startTime = new Date();
+    }
+
+    if (inp > ans) {
+        alert("太大了，再試一次");
+        guesstime++;
+    }
+    else if (inp < ans) {
+        alert("太小了，再試一次");
+        guesstime++;
+    }
+    else {
+        let endTime = new Date();
+        let diffSec = Math.floor((endTime - startTime) / 1000);
+        alert("恭喜猜對，共猜了 " + guesstime + " 次，耗時 " + diffSec + " 秒");
+
+        guesstime = 1;
+        ans = Math.floor(Math.random() * 101);
+    }
+}
